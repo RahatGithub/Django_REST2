08af15bb60524627789d99e48438dd117bf228f0
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-04-27 22:21:43 +0600

    views.departmentApi added

---
+++ EmployeeApp/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers 
+from EmployeeApp.models import Departments, Employees 
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departments
+        fields = (
+            'DeptId',
+            'DeptName'
+        )
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields = (
+            'EmployeeId',
+            'EmployeeName',
+            'Department',
+            'DateOfJoining',
+            'PhotoFileName'
+        )
--- EmployeeApp/views.py
+++ EmployeeApp/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser 
+from django.http.response import JsonResponse
 
-# Create your views here.
+from EmployeeApp.models import Departments, Employees
+from EmployeeApp.serializers import DepartmentSerializer, EmployeeSerializer 
+
+
+@csrf_exempt
+def departmentApi(request, id=0):
+    if request.method == 'GET':
+        departments = Departments.objects.all()
+        department_serializer = DepartmentSerializer(departments, many=True)
+        return JsonResponse(department_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        department_data = JSONParser().parse(request)
+        department_serializer = DepartmentSerializer(data=department_data)
+        if department_serializer.is_valid():
+            department_serializer.save() 
+            return JsonResponse("Added successfully", safe=False)
+        return JsonResponse("Failed to add", safe=False)
+    
+    elif request.method == 'PUT':
+        department_data = JSONParser().parse(request)
+        department = Departments.objects.get(DeptId=department_data['DeptId'])
+        department_serializer = DepartmentSerializer(department, data=department_data)
+        if department_serializer.is_valid():
+            department_serializer.save()
+            return JsonResponse("Updated successfully", safe=False)
+        return JsonResponse("Failed to update", safe=False) 
+    
+    elif request.method == 'DELETE':
+        department = Departments.objects.get(DeptId=id)
+        department.delete()
+        return JsonResponse("Deleted successfulyy", safe=False)
